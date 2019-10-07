92b36505af15ba3a873b97c224117d04576f3e06
schwarzblade <schwarzblade@gmail.com>
2019-10-07 21:02:23 +0200

    first commit

---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import { recipe } from  '../tempDetails';
+export default class RecipeDetails extends
+Component {
+	render(){
+		return (
+			<React.Fragment><h1>Hello From Details</h1></React.Fragment>
+		);
+	}
+}
---
+++ src/components/RecipeSearch.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+
+export default class RecipeSearch extends Component {
+	render() {
+		return (
+			<React.Fragment><h1>Hello From Recipe Search</h1></React.Fragment>
+		)
+	}
+}
