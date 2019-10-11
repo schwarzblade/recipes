d82a58d9e92f4a707ef154922749b5dc6e8ec4c2
schwarzblade <schwarzblade@gmail.com>
2019-10-11 18:13:58 +0200

    Updates

---
+++ precache-manifest.935e9ffc07553e4e7fe8ee349305974b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e3907abe3e9ad6986977f745aa04bb",
+    "url": "/schwarzblade/recipes/index.html"
+  },
+  {
+    "revision": "b97f15c7028368d9fe22",
+    "url": "/schwarzblade/recipes/static/css/main.3285b26d.chunk.css"
+  },
+  {
+    "revision": "6b891d4fb7af39d8ed49",
+    "url": "/schwarzblade/recipes/static/js/2.71a8bca9.chunk.js"
+  },
+  {
+    "revision": "b97f15c7028368d9fe22",
+    "url": "/schwarzblade/recipes/static/js/main.8c811a78.chunk.js"
+  },
+  {
+    "revision": "c799ed74f1601da5167b",
+    "url": "/schwarzblade/recipes/static/js/runtime-main.4609d178.js"
+  }
+]);
